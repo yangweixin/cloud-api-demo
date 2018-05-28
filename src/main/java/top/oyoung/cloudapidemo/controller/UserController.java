ce7f2b36ddc1ad48787da63371eb6a8cec8d7860
yangweixin <yangwx@ejiayou.com>
2018-05-29 01:46:30 +0800

    config client eureka

--- src/main/java/top/oyoung/cloudapidemo/controller/UserController.java
+++ src/main/java/top/oyoung/cloudapidemo/controller/UserController.java
@@ -2,6 +2,7 @@ package top.oyoung.cloudapidemo.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import top.oyoung.cloudapidemo.service.remote.UserService;
 
 import javax.annotation.Resource;
 
+@RefreshScope
 @RequestMapping("/user")
 @RestController
 public class UserController {
